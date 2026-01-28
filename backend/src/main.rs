8f7f60b6ca565c8f05e33975e3785d08da73846c
Tomkoid <tomaszierl@outlook.com>
2026-01-28 09:48:55 +0100

    fix: cors shit
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,16 @@
 mod models;
 mod routes;
 
-use axum::Router;
+use axum::{
+    Router,
+    http::{
+        HeaderValue,
+        header::{AUTHORIZATION, CONTENT_TYPE},
+    },
+};
 use sqlx::postgres::PgPoolOptions;
 use std::env;
+use tower_http::cors::Any;
 
 use crate::db::ensure_database_exists;
 
@@ -34,8 +41,18 @@ pub async fn main() -> color_eyre::Result<()> {
 
     let state = routes::AppState { db: pool };
 
+    let cors_layer = tower_http::cors::CorsLayer::new()
+        .allow_methods(Any)
+        .allow_origin(
+            "https://utilbench-api.tomkoid.cz"
+                .parse::<HeaderValue>()
+                .unwrap(),
+        )
+        .allow_headers([AUTHORIZATION, CONTENT_TYPE]);
+
     let app = Router::new()
         .nest("/api/v1", routes::api_routes())
+        .layer(cors_layer)
         .with_state(state);
 
     let bind_addr = format!("{}:{}", host, port);
